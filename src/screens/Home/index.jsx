40edfc77c656a1b74b86e0e3ca3d5361dad33f7d
aleanrocha <alean.oficial.me@gmail.com>
2024-05-28 11:47:01 -0300

    add netflix promotion box

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -2,12 +2,25 @@ import { Fragment } from 'react'
 
 import { Banner } from '../../components/Banner'
 import { Header } from '../../components/Header'
+import { PopcornIcon } from '../../components/Icons'
 
 export const Home = () => {
   return (
     <Fragment>
       <Header />
       <Banner />
+      <div className="bg-box-radial-mobile-flix md:bg-box-radial-desktop-flix w-11/12 md:w-4/5 mx-auto mt-4 p-6 pt-16 md:pt-6 rounded-md flex flex-col md:flex-row gap-2 md:gap-8 md:justify-center relative md:-translate-y-2/4">
+        <PopcornIcon />
+        <div className="flex flex-col gap-2">
+          <h3 className="text-xl font-bold">
+            A Netflix que você adora por apenas R$ 20,90.
+          </h3>
+          <p>Assine o plano Padrão com anúncios.</p>
+          <a href="#" className="text-link-flix underline">
+            Saiba mais
+          </a>
+        </div>
+      </div>
     </Fragment>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,11 @@ export default {
       backgroundImage: {
         'banner-home-flix': 'url("./src/assets/bg-login-flix.jpg")',
         'banner-mask-flix':
-          'linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%)'
+          'linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%)',
+        'box-radial-mobile-flix':
+          'radial-gradient( 120.35% 220% at 49.86% -6.29%, #e50914 0%, #0e1b4f 46.15%, #0d1121 100% )',
+        'box-radial-desktop-flix':
+          'radial-gradient( 51.39% 511.66% at 47.68% -217.91%, #ff9900 0%, #e50914 17.27%, #0e1b4f 79.44%, #000413 100% )'
       }
     }
   },
