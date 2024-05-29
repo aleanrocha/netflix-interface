ff059294b45b31bce3c550cdde9b37913347ce33
aleanrocha <alean.oficial.me@gmail.com>
2024-05-29 12:19:08 -0300

    add video sections

---
+++ src/components/VideoFeature/index.jsx
@@ -0,0 +1,36 @@
+export const VideoFeature = ({
+  title,
+  content,
+  image,
+  altImage,
+  video,
+  isReverse
+}) => {
+  return (
+    <section className="border-b-8 border-highlight-flix border-opacity-50">
+      <div
+        className={`flex flex-col md:flex-row justify-center items-center gap-4 md:gap-16 py-16 px-4 max-w-7xl m-auto ${isReverse ? 'md:flex-row-reverse' : ''}`}
+      >
+        <div className="w-full max-w-lg text-center md:text-left">
+          <h2 className="text-4xl md:text-5xl font-bold">{title}</h2>
+          <p className="mt-4 text-lg md:text-2xl">{content}</p>
+        </div>
+        <div className="w-full max-w-lg">
+          <div className="relative">
+            <video
+              src={video}
+              autoPlay
+              muted
+              loop
+              className="absolute top-10 -z-10"
+            ></video>
+            <img
+              src={image}
+              alt={altImage}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,15 +1,19 @@
 import { Fragment } from 'react'
 
+import tvFlix from '../../assets/tv-flix.png'
 import { Banner } from '../../components/Banner'
 import { Header } from '../../components/Header'
 import { PopcornIcon } from '../../components/Icons'
+import { VideoFeature } from '../../components/VideoFeature'
+import rodolfoVideo from '../../videos/rodolfo.mp4'
+import tiringaVideo from '../../videos/tiringa.mp4'
 
 export const Home = () => {
   return (
     <Fragment>
       <Header />
       <Banner />
-      <div className="bg-box-radial-mobile-flix md:bg-box-radial-desktop-flix w-11/12 md:w-4/5 mx-auto mt-4 p-6 pt-16 md:pt-6 rounded-md flex flex-col md:flex-row gap-2 md:gap-8 md:justify-center md:-mt-[-72px] md:relative">
+      <div className="bg-box-radial-mobile-flix md:bg-box-radial-desktop-flix w-11/12 md:w-4/5 mx-auto mt-4 p-6 pt-16 md:pt-6 rounded-md flex flex-col md:flex-row gap-2 md:gap-8 md:justify-center md:-mt-[72px] md:relative">
         <PopcornIcon />
         <div className="flex flex-col gap-2">
           <h3 className="text-xl font-bold">
@@ -21,6 +25,25 @@ export const Home = () => {
           </a>
         </div>
       </div>
+      <VideoFeature
+        title={'Aproveite na TV'}
+        content={
+          'Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray e outros dispositivos.'
+        }
+        image={tvFlix}
+        altImage={'Imagem Smart TV'}
+        video={rodolfoVideo}
+      />
+      <VideoFeature
+        title={'Assista onde quiser'}
+        content={
+          'Assista a quantos filmes e séries quiser no celular, tablet, laptop e TV.'
+        }
+        image={tvFlix}
+        altImage={'Imagem Desktop'}
+        video={tiringaVideo}
+        isReverse
+      />
     </Fragment>
   )
 }
