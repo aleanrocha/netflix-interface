13264fd4c5536d3011a46194377fdf01f2a73a32
aleanrocha <alean.oficial.me@gmail.com>
2024-05-29 11:44:17 -0300

    change netflix promotion box

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
     <Fragment>
       <Header />
       <Banner />
-      <div className="bg-box-radial-mobile-flix md:bg-box-radial-desktop-flix w-11/12 md:w-4/5 mx-auto mt-4 p-6 pt-16 md:pt-6 rounded-md flex flex-col md:flex-row gap-2 md:gap-8 md:justify-center relative md:-translate-y-2/4">
+      <div className="bg-box-radial-mobile-flix md:bg-box-radial-desktop-flix w-11/12 md:w-4/5 mx-auto mt-4 p-6 pt-16 md:pt-6 rounded-md flex flex-col md:flex-row gap-2 md:gap-8 md:justify-center md:-mt-[-72px] md:relative">
         <PopcornIcon />
         <div className="flex flex-col gap-2">
           <h3 className="text-xl font-bold">
