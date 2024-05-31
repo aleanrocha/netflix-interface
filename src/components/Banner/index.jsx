be2f33d408dcb16d5de7869a5ded51f5e9008c84
aleanrocha <alean.oficial.me@gmail.com>
2024-05-31 12:21:41 -0300

    update pathname

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -8,7 +8,7 @@ export const Banner = () => {
   return (
     <section className="bg-banner-home-flix bg-top bg-cover w-screen h-max-90 md:h-screen overflow-hidden relative top-0 left-0 z-0 flex justify-center items-center">
       <span className="bg-black-overlay absolute top-0 left-0 w-full h-screen"></span>
-      {pathname === '/netflix-interface' && (
+      {pathname === '/netflix-interface/' && (
         <>
           <span className="bg-banner-mask-flix absolute top-0 left-0 w-full h-screen"></span>
           <div className="z-10 text-center p-8 flex flex-col gap-4">
